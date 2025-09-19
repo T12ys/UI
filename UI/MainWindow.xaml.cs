fad80bc6dc157e73931e1d62454ca2a28e55ed5b
T12ys <155729923+T12ys@users.noreply.github.com>
2025-09-19 10:08:58 +0400

    Первый коммит

---
+++ UI/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+
+namespace UI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void StartProgram(object sender, RoutedEventArgs e)
+    {
+        string secondAppPath = @"C:\Users\User\RiderProjects\Entity Framework\SecondProgram\SecondProgram\bin\Debug\net9.0\SecondProgram.exe";
+        string debug = DebugCheckBox.IsChecked == true ? "true" : "false";
+        Process.Start(secondAppPath, new string[] { "1", "6", @"C:\Users\User\RiderProjects\Entity Framework\UI\UI\Logs.txt" , debug });
+        Application.Current.Shutdown();
+    }
+}
